de97e9f70c9b95d4a804d2ab80f03cfbe4228b2b
kafri8889 <kafri8889@gmail.com>
2023-09-05 12:38:19 +0700

    Add bubble sort docs

--- src/sorting/BubbleSort.kt
+++ src/sorting/BubbleSort.kt
@@ -1,17 +1,40 @@
 package sorting
 
+/**
+ * Swap value dari index i ke j, dan index j ke i
+ */
 private fun swap(arr: Array<Int>, i: Int, j: Int) {
     arr[i] = arr[j].also { arr[j] = arr[i] }
 }
 
-fun bubbleSort(arr: Array<Int>) {
+/**
+ * Cek apakah kedua nilai [n1] dan [n2] harus ditukar
+ *
+ * @param n1 nilai ke-1
+ * @param n2 nilai ke-2
+ * @param ascending sort order
+ *
+ * @return true jika kedua nilai tersebut harus ditukar, false otherwise
+ */
+private fun shouldSwap(n1: Int, n2: Int, ascending: Boolean): Boolean {
+    return if (ascending) n1 < n2 else n1 > n2
+}
+
+/**
+ * @param arr array
+ * @param ascending sort order, ascending (true) smallest to greatest, descending (false) otherwise
+ */
+fun bubbleSort(arr: Array<Int>, ascending: Boolean = true) {
     if (arr.size <= 1) return
 
     while (true) {
+        // Pointer 2
         var p2 = 1
+        // Untuk pengecekan jika swapped == false, berarti semua nilainya sudah di sort, maka hentikan loop
         var swapped = false
+        // p1: Pointer 1
         for (p1 in arr.indices) {
-            if ((arr.getOrNull(p2) ?: break) < arr[p1]) swap(arr, p1, p2).also { swapped = true }
+            if (shouldSwap((arr.getOrNull(p2) ?: break), arr[p1], ascending)) swap(arr, p1, p2).also { swapped = true }
             p2++
         }
         if (!swapped) break
